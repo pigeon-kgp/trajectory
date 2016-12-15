1060fdb65c1a76eebdd06a6e19f79ed4e2be9375
mEhuL <Mul16@users.noreply.github.com>
2016-12-16 00:15:33 +0530

    Working :)

--- bbox.py
+++ bbox.py
@@ -1,11 +1,11 @@
-
 import os
 import numpy as np
 import cv2
-img = np.zeros((1000, 1000, 3), np.uint8)
+img = np.zeros((700, 700, 3), np.uint8)
+#cv2.imshow("image",img)
 #1131,2008-02-02 13:30:49,116.45804,39.86973
 #latmin=180;lonmin=180;latmax=-180;lonmax=-18000, 3), np.uint8)
-path=raw_input("Enter Path")
+path=raw_input("Enter Path\n")
 folderList=os.listdir(path+'/')
 for folders in folderList:
     os.chdir(path+'/'+folders+'/')
@@ -28,13 +28,20 @@ for folders in folderList:
                         if (count==2): com1=i
                         if (count==3): com2=i; break
                 try:
-                    lat=(float(line[com1+1:com2])-int(line[com1+1:com2]))*100
-                    lon=(float(line[com2+1:])-int(line[com2+1:]))*100
+                    lat=float(line[com1+1:com2])
+                    lon=float(line[com2+1:])
+
+                    latitude=int((lat-int(lat))*700)
+                    longitude=int((lon-int(lon))*700)
+                    #print("Merry Chritmas")
 
                 except:
                     print("Some error occured. Ignoring error.")
                     continue   
-                img[lat,lon]+=1            
+                img[latitude,longitude][0]+=10          
+                img[latitude,longitude][1]+=10
+                img[latitude,longitude][2]+=10
+
          #   if (lat==0 or lon==0): print(counter); break
          #   if (lat>latmax): latmax=lat
          #   if (lat<latmin): latmin=lat
@@ -48,5 +55,9 @@ for folders in folderList:
 #    name=input("Pl. enter name of file without extension: ")
 #    f=open(name+".txt","w")
 #    f.write("Minimum: "+latmin+", "+lonmin+"\nMaximum: "+latmax+", "+lonmax)                                         
-img.imshow()
-a=input()
+cv2.imshow("image",img)
+while(1):
+    a=cv2.waitKey(1)
+    if(a=='27'):
+        break
+
