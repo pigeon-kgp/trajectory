badd4bc793be8318f32572c92c34aa9015307adc
americast <sayan.sinha@iitkgp.ac.in>
2016-12-15 12:29:17 +0530

    Working great!

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 trajectory Planning
+
+place data in the same folder as the clone of this
--- seg.py
+++ seg.py
@@ -1,67 +1,47 @@
 import glob
 import os
-#366,2008-02-02 13:30:48,116.45353,39.90732
-#6656,2008-02-02 13:38:10,116.36422,39.95658
 def timeonly(a_list):
     return a_list[1]
 def seg(hours):
-    time=0
+    time_out=0
     if (24%hours!=0): print("Hours can only be factors of 24."); return
     while(True):
-        time+=hours
+        time_out+=hours
         parentlist=[]
         broken=0
         for i in range(1,15):
             loc="data/0"+str(i)+"/*"
             for file in glob.glob(loc):
-                print ("Processing "+file+" located in "+loc)
+                print ("Processing "+file)
                 myfile = open(file, "r")
-                flocate=open("pos.txt","r")
-                currpos=0
                 while(True):
-                    linehere=flocate.readline()
-                    if not linehere: break
-                    if (linehere.find(str(file))):
-                      for j in range(len(linehere)):
-                          if (linehere[j]==" "):  currpos=int(linehere[j+1:])
-                      break
-                flocate.close()
-                myfile.seek(currpos)
-                lines = myfile.readlines()
-                if not lines:
-                    myfile.close()
-                    broken+=1
-                    continue
-                for line in lines:
-                    if (line[3]==","):
-                        date=line[4:14]
-                        time=int(line[15:17])*3600+int(line[18:20])*60+int(line[21:23])+24*3600*(int(line[12:14])-2)
-                    else:
-                         date=line[5:15]
-                         time=int(line[16:18])*3600+int(line[19:21])*60+int(line[22:24])+24*3600*(int(line[13:15])-2)                       
-                    parentlist.append([line,time,date])
-                    if (time>=3600*hours):
-                        pos=myfile.tell()
+                    line = myfile.readline()
+                    if not line:
                         myfile.close()
-                        myfile=open("pos.txt","r+")
-                        while(True):
-                            currpos=myfile.tell()
-                            linehere=myfile.readline()
-                            if not linehere: break
-                            if (linehere.find(str(file))):
-                                break
-                        myfile.seek(currpos)
-                        myfile.write(str(file)+": "+str(pos)+"\n")
+                        broken+=1
+                        break
+                    try:
+                      for k in range(len(line)):
+                          if (line[k]==","): break
+                      date=line[k+1:k+11]
+                      time=int(line[k+12:k+14])*3600+int(line[k+15:k+17])*60+int(line[k+18:k+20])+24*3600*(int(line[k+9:k+11])-2)
+                    except:
+                        print ("Some error has occurred. Ignoring error.")
+                        continue
+                    if (time>=3600*(time_out-hours) and time <=time_out*3600):   
+                      parentlist.append([line,time,date])
+                    if (time>=3600*time_out):
                         myfile.close()
                         break
         if (broken==14): break
         parentlist.sort(key=timeonly) 
-        os.system("mkdir "+(parentlist[0])[2])
-        os.system("cd "+(parentlist[0])[2])
-        os.system("cd ../")
-        towrite=file((parentlist[0])[2]+"/"+time,"a")
-        for i in parentlist:
-            towrite.write(i[0]+"\n")
+        if (len(parentlist)>0):
+          os.system("mkdir "+str((parentlist[0])[2]))
+          time_print=(time_out)-(int(str((parentlist[0])[2])[8:10])-2)*24
+          time_print=str(time_print-hours)+":00 - "+str(time_print)+":00"
+          towrite=open(str ((parentlist[0])[2])+"/"+time_print+".txt","a")
+          for i in parentlist:
+            towrite.write(i[0])
 
 n=int(input("Pl enter hour segments: "))
 temp=open("pos.txt","w")
