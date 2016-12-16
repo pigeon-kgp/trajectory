d3dbe6567f793a2d71d76fa53b9ff426c8df8878
Sayan Sinha <sayan.sinha@iitkgp.ac.in>
2016-12-16 15:20:29 +0530

    Segragation updated

--- Segregation.py
+++ Segregation.py
@@ -18,7 +18,7 @@ def seg(hours):
     while(True):
         time_out+=hours
         parentlist=[]
-        broken=0
+        brakeflag=1
 
         
         for folders in folderList:
@@ -26,14 +26,30 @@ def seg(hours):
             os.chdir(filePath)
             fileList=os.listdir(filePath)
             for file in fileList:
-                print(filePath+file)
-                #print ("Extracting.. "+path+file)
+                print("Extracting "+filePath+file)
                 myfile = open(filePath+file, "rt")
+                flocate.seek(0)
+                fpos=0; flag=1
+                fposinline=0
                 while(True):
+                    fpos=flocate.tell()
+                    fline=flocate.readline()
+                    if not fline: flag=0; break
+                    if (str(myfile) in fline):
+                        for i in xrange(len(fline)):
+                            if(fline[i]==" "):
+                                fposinline=int(fline[i+1:])
+                                break
+                        break
+                if (flag==1):
+                    myfile.seek(fposinline)
+                else:
+                    myfile.seek(0)
+                while(True):
+                    pos=myfile.tell()
                     line = myfile.readline()
                     if not line:
                         myfile.close()
-                        broken+=1
                         break
                     try:
                       time=timeonly(line)
@@ -43,18 +59,28 @@ def seg(hours):
                     if (time>=3600*(time_out-hours) and time <=time_out*3600):
                         parentlist.append(line)
                     if (time>=3600*time_out):
+                        brakeflag=0
                         myfile.close()
+                        fpos=0
+                        flocate.seek(0)
+                        while(True):
+                            fpos=flocate.tell()
+                            flineagain=flocate.readline()
+                            if not flineagain: break
+                            if (str(myfile) in flineagain): break
+                        flocate.seek(fpos)
+                        flocate.write(str(file)+" "+str(pos)+"\n")
                         break
-        if (broken==14): 
+        if (brakeflag==1): 
             break
         parentlist.sort(key=timeonly) 
         if (len(parentlist)>0):
             os.chdir(OutputPath+'/')
             try:
                 for k in range(len(parentlist[0])):
-                    if (line[k]==","):
+                    if ((parentlist[0])[k]==","):
                               break
-                datenow=line[k+1:k+11]
+                datenow=(parentlist[0])[k+1:k+11]
                 os.mkdir(datenow)
             except:
                 print("Directory already exists :-) !")
@@ -62,10 +88,13 @@ def seg(hours):
             time_print=(time_out)-(int(datenow[8:10])-2)*24
             time_print=str(time_print-hours)+":00 - "+str(time_print)+":00"
             towrite=open(OutputPath+'/'+datenow+"/"+time_print+".txt","a")
-            for i in parentlist:   
-                towrite.write(i[0])
+            for i in parentlist:
+                towrite.write(i)
 
 n=int(raw_input("Pl enter hour segments: "))
-temp=open("pos.txt","w")
-temp.close()
+flocate=open("pos.txt","w")
+flocate.close()
+flocate=open("pos.txt","r+")
 seg(n)
+print("\nDone!")
+flocate.close()
