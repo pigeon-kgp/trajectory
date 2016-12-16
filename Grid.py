814aaa87b9b7e92583c3ba687bba5d63ba8b7b97
mEhuL <Mul16@users.noreply.github.com>
2016-12-16 07:08:06 +0530

    update

--- Grid.py
+++ Grid.py
@@ -1,11 +1,16 @@
 import os
 import numpy as np
 import cv2
+point1=[116.10041,88322]
+point2=[117.10539,40.11289]
 def Grid(point1,point2):#point1 point2 are list
     path=raw_input("Enter Path\n")
     OutputPath=raw_input("Enter Output src\n")
     folderList=os.listdir(path+'/')
-    lon,lat=int(raw_input())
+    
+    lon=int(raw_input())
+    lat=int(raw_input())
+    
     loniterator=(point2[0]-point1[0])/lon
     latiterator=(point2[1]-point1[1])/lat
 
@@ -25,14 +30,14 @@ def Grid(point1,point2):#point1 point2 are list
         #os.chdir(path+'/'+folders+'/')
         fileList=os.listdir(path+'/'+folders+'/')
         for files in fileList:
-            with open(files,"rt") as InputFile:        
-                myfile = open(path+'/'+folders+'/'+files, "r")
+            with open(path+'/'+folders+'/'+files,"rt") as InputFile:        
+                #myfile = open(path+'/'+folders+'/'+files, "r")
                 print("Extracting.."+path+'/'+folders+'/'+files)
                 for lines in InputFile:
                     c=0
                     for i in range(len(lines)):
                         
-                        if(line[i]==','):
+                        if(lines[i]==','):
                             c+=1
                         if(c==1):
                             lonBegin=i+2
@@ -44,14 +49,21 @@ def Grid(point1,point2):#point1 point2 are list
                     lonData=float(lines[lonBegin:lonEnd])
                     latData=float(lines[latBegin:latEnd])
                     
-                    print(lonData,latData)
-                    
+                    #print(lonData,latData)
+                    c=0
                     for i in range(0,lat-1):
                         for j in range(0,lon-1):
+                            
                             if(lonData>=grid[str(i)+str(j)][0] and lonData<grid[str(i+1)+str(j+1)][0]):
                                 if(latData>=grid[str(i)+str(j)][1] and latData<grid[str(i+1)+str(j+1)][1]):
                                     key=str(i)+str(j)
-                    filePointerList[key].write(lines)
+                                    c=1
+
+
+                    if(c):
+                        
+                        filePointerList[key].write(lines)
+Grid(point1,point2)
                 
 
 
