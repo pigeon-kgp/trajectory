26f74cabf9f1243c212ba6b4748f94db4a04730e
americast <sayan.sinha@iitkgp.ac.in>
2016-12-21 11:16:11 +0530

    Flow working

--- Grid.py
+++ Grid.py
@@ -5,7 +5,8 @@ point1=[115.96198,39.75903]#test case
 point2=[117.10539,40.11289]
 def Grid(point1,point2):#point1 point2 are list
     path=raw_input("Enter Path\n")
-    OutputPath=raw_input("Enter Output src\n")
+    global OutputPath
+    OutputPath=raw_input("Enter Output path\n")
     folderList=os.listdir(path+'/')
     
     lon=int(raw_input("Enter Columns : "))
@@ -27,14 +28,15 @@ def Grid(point1,point2):#point1 point2 are list
 
     for i in range(0,lat):
         for j in range(0,lon):
-            filePointerList[str(i)+str(j)]=open(str(i)+"x"+str(j)+".txt",'a')
+            filePointerList[str(i)+str(j)]=open(str(int(str(i))+1)+"x"+str(int(str(j))+1)+".txt",'a')
             grid[str(i)+str(j)]=[point1[0]+i*loniterator,point1[1]+j*latiterator]
 
     for folders in folderList:
         #os.chdir(path+'/'+folders+'/')
         fileList=os.listdir(path+'/'+folders+'/')
-        for files in fileList:
-            with open(path+'/'+folders+'/'+files,"rt") as InputFile:        
+        try:
+          for files in fileList:
+              with open(path+'/'+folders+'/'+files,"rt") as InputFile:        
                 #myfile = open(path+'/'+folders+'/'+files, "r")
                 print("Extracting.."+path+'/'+folders+'/'+files)
                 totalCost=0
@@ -93,4 +95,81 @@ def Grid(point1,point2):#point1 point2 are list
                     if(d):
                         filePointerList[key].write(lines)
                 print(files+".........complete.....")
-Grid(point1,point2)
+
+        except:
+              print "Some minor error occured."
+def Assess():
+    #1409,2008-02-02 13:33:45,116.34732,39.8881
+    grno=raw_input("Which grid would you like to assess?: ")
+    myfile=open(OutputPath+"/Grid/"+grno+".txt","r")
+    dt=int(raw_input("Day?: "))
+    sh=int(raw_input("Start hour?: "))
+    eh=int(raw_input("End hour?: "))
+    cars={}
+    while(True):
+        line=myfile.readline()
+        if not line: break
+        info=line.split(",")
+        carno=info[0]
+        day=int((info[1])[8:10])
+        time=(info[1])[11:]; hr=int(time[0:2])
+        time=int(time[0:2])*3600+int(time[3:5])*60+int(time[6:])
+        if (hr>=sh and hr<eh and day==dt):
+            try:
+                if time<(cars[carno])[0]: (cars[carno])[0]=time
+                if time>(cars[carno])[1]: (cars[carno])[1]=time
+            except:
+                cars[carno]=[time,time]
+    print "Taxis present:"
+    for i in cars: print i
+    myfile.close()
+    gpr=int(grno[0]); gpo=int(grno[2])
+    inflow=[]; outflow=[]
+    for i in xrange(gpr-1,gpr+2):
+        for j in xrange(gpo-1,gpo+2):
+            if (i==gpr and j==gpo): continue
+            try:
+                myfile=open(OutputPath+"/Grid/"+str(i)+"x"+str(j)+".txt","r")
+            except: continue
+            while(True):
+                line=myfile.readline()
+                if not line: break
+                info=line.split(",")
+                carno=info[0]
+                day=int((info[1])[8:10])
+                time=(info[1])[11:]; hr=int(time[0:2])
+                if (hr>=sh and hr<eh and day==dt):
+                    try:
+                        if time<(cars[carno])[0]:
+                            flag=1
+                            for k in inflow:
+                                if carno==k: flag=0; break
+                            if flag==1: inflow.append(carno)
+                        if time>(cars[carno])[1]:
+                            flag=1
+                            for k in outflow:
+                                if carno==k: flag=0; break
+                            if flag==1: outflow.append(carno)
+                    except:
+                        continue
+
+    print "Inflow:"
+    for i in inflow: print i
+    print "Outflow:"
+    for i in outflow: print i                
+
+
+    
+
+
+
+
+OutputPath="."
+while(True):
+    print "What would you like to do next?\n1) Create Grid\n2) Assess flow.\n3) Exit.\nPlease enter choice: "
+    choice=int(raw_input())
+    os.system("clear")
+    if (choice==1): Grid(point1,point2)
+    if (choice==2): Assess()
+    if (choice==3): break
+    else: print "Invalid choice."
